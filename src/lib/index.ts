4d97474b13ab51b4e680b979008d53151dfc9507
Jaroslav Khorishchenko <websnipter@gmail.com>
2021-04-01 13:24:24 +0300

    Mod

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,10 @@
 import { existsSync, readFileSync, writeFileSync } from 'fs';
-import { CliOpt, getArgsVersionParam, getArgsVersionParamOrErr, log, UnknownParsedArgs } from 'utils';
+import { CliOpt, getArgsStrParam, getArgsVersionParam, getArgsVersionParamOrErr, log, UnknownParsedArgs } from 'utils';
+import { dateToFormatedStr } from 'utils/date';
 
 import { changelogToStr, sectionsToStr } from './generator';
 import { strToChangelog } from './parser';
-import { Changelog } from './types';
+import { Changelog, Version } from './types';
 import { argsToNewSectionItem, getSectionsWithVersion, mergeSections } from './utils';
 
 const defVersionParam = 'Unreleased';
@@ -14,6 +15,9 @@ export const processCmd = (cmd: string, args: UnknownParsedArgs, opt: CliOpt) =>
   }
   if (cmd === 'add') {
     return processAddCmd(args, opt);
+  }
+  if (cmd === 'change') {
+    return processChangeCmd(args, opt);
   }
   if (cmd === 'remove') {
     return processRemoveCmd(args, opt);
@@ -45,6 +49,42 @@ const processAddCmd = (args: UnknownParsedArgs, opt: CliOpt) => {
   const versions = changelog.versions.map(version =>
     version.name !== verParam ? version : { ...version, sections: mergeSections([...version.sections, newSection]) },
   );
+
+  writeChangelogToFile(opt, { ...changelog, versions });
+};
+
+const processChangeCmd = (args: UnknownParsedArgs, opt: CliOpt) => {
+  const changelog = readChangelogFromFile(opt);
+  const verParam = getArgsVersionParamOrErr(args);
+  const newNameParam = getArgsStrParam(args, ['name', 'n']);
+  const newDateParam = getArgsStrParam(args, ['date', 'd']);
+
+  if (!newNameParam && !newDateParam) {
+    throw new Error('Name or date param required');
+  }
+
+  const curVer = changelog.versions.find(itm => itm.name === verParam);
+  if (!curVer) {
+    throw new Error(`Version "${verParam}" not found`);
+  }
+
+  const versions = changelog.versions.map(version => {
+    if (version.name !== verParam) {
+      return version;
+    }
+    let newVersion: Version = version;
+    if (newNameParam) {
+      newVersion = { ...newVersion, name: newNameParam };
+    }
+    if (newDateParam) {
+      if (['current', 'cur'].includes(newDateParam)) {
+        newVersion = { ...newVersion, date: dateToFormatedStr(new Date()) };
+      } else {
+        newVersion = { ...newVersion, date: newDateParam };
+      }
+    }
+    return newVersion;
+  });
 
   writeChangelogToFile(opt, { ...changelog, versions });
 };
